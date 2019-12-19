23d68942a6ea52a0366715862099326ec71b70fd
Isai <isai.ds18@gmail.com>
2019-12-18 23:49:02 -0600

    Added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    sfdx force:org:create -v DevHubTrail -s -f config/project-scratch-def.json -a ciorg --wait 3
-    sfdx force:org:display -u ciorg
-    sfdx force:source:push -u ciorg
-    sfdx force:apex:test:run -u ciorg --wait 10
-    sfdx force:org:delete -u ciorg -p
-else
-    sfdx force:source:deploy -u DevHubTrail --testlevel RunLocalTests -p force-app
-fi
+
+sfdx force:source:deploy -u DevHubTrail --testlevel RunLocalTests -p force-app
